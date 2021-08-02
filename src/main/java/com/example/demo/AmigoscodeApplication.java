9273fda19f2581e8a13ccc7390ed029bca0e6a74
unknown <ummitisuresh123@gmail.com>
2021-08-02 12:33:52 +0530

    initial setup

---
+++ src/main/java/com/example/demo/AmigoscodeApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AmigoscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscodeApplication.class, args);
+	}
+
+	/*
+	 * @GetMapping("/") public String hello() { return "Hello World"; }
+	 */
+	
+	@GetMapping("/")
+	public List<String> hello() {
+		return Arrays.asList("Hello", "World");
+	}
+	
+	
+	
+}
