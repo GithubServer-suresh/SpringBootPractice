daf9f8ce734e066ed3ba4e2e7f0f63edba8a2704
unknown <ummitisuresh123@gmail.com>
2021-08-02 18:57:24 +0530

    Commiting

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -2,6 +2,13 @@ package com.example.demo.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +17,13 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Entity
+@Table
 public class Student {
 
+	@Id
+	@SequenceGenerator(name = "student_seq", sequenceName = "student_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_seq")
 	private Long id;
 	private String name;
 	private String email;
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,21 +1,20 @@
 package com.example.demo.service;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
 @Service
 public class StudentService {
+	
+	@Autowired
+	private StudentRepository studentRepository;
 
 	
 	public List<Student> hello(){
-		return Arrays.asList(
-				new Student(1L,"Mariam","Mariam.jamal@gmail.com",LocalDate.of(2000, Month.JANUARY, 5),21)
-				//new Student(1L,"Mariam","Mariam.jamal@gmail.com",LocalDate.of(2001, Month.FEBRUARY, 5),22)
-				);
+		return studentRepository.findAll();
 	}
 }
