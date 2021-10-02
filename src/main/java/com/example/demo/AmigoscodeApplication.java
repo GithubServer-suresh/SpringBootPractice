5d84263c4ac794000c8c5cc120b372421af26551
unknown <ummitisuresh123@gmail.com>
2021-10-02 10:55:21 +0530

    added method

--- src/main/java/com/example/demo/AmigoscodeApplication.java
+++ src/main/java/com/example/demo/AmigoscodeApplication.java
@@ -2,14 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AmigoscodeApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AmigoscodeApplication.class, args);
 		System.out.println("Spring boot Main class");
+	}
+	
+	public void test() {
+		System.out.println("test method");
 	}
 	
 }
