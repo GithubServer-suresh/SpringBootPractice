6512a014f1a68e0525562ec5fb5010ed79bc5be6
GithubServer-suresh <ummitisuresh123@gmail.com>
2021-10-02 10:30:34 +0530

    Update AmigoscodeApplication.java

--- src/main/java/com/example/demo/AmigoscodeApplication.java
+++ src/main/java/com/example/demo/AmigoscodeApplication.java
@@ -9,27 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AmigoscodeApplication {
 
 	public static void main(String[] args) {
-		System.out.println("This is from STS");
-		System.out.println("This is main class added by dev");
 		SpringApplication.run(AmigoscodeApplication.class, args);
 	}
-
-	/*
-	 * @GetMapping("/") public String hello() { return "Hello World"; }
-	 */
-	
-	/*
-	 * @GetMapping("/") public List<String> hello() { return Arrays.asList("Hello",
-	 * "World"); }
-	 */
-	
-	/*
-	 * @GetMapping("/") public List<Student> hello(){ return Arrays.asList( new
-	 * Student(1L,"Mariam","Mariam.jamal@gmail.com",LocalDate.of(2000,
-	 * Month.JANUARY, 5),21) //new
-	 * Student(1L,"Mariam","Mariam.jamal@gmail.com",LocalDate.of(2001,
-	 * Month.FEBRUARY, 5),22) ); }
-	 */
-	
 	
 }
